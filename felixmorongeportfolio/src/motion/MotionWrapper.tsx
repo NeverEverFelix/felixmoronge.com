c0dda531c34d80a0e7b9cd28ca61e5cf89b35823
NeverEverFelix <felixmftc@icloud.com>
2025-05-10 20:37:59 -0400

    animations additions

---
+++ felixmorongeportfolio/src/motion/MotionWrapper.tsx
@@ -0,0 +1,28 @@
+// src/motion/variants.ts
+
+export const fadeInUp = {
+    hidden: { opacity: 0, y: 40 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    },
+  };
+  
+  export const staggerContainer = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+  
+  export const fadeInItem = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+  
