70f893909813c102785eb036238fa1418a0f4e14
NeverEverFelix <felixmftc@icloud.com>
2025-05-13 19:19:13 -0400

    MVP Portfolio finished

--- felixmorongeportfolio/src/components/ContentCard.tsx
+++ felixmorongeportfolio/src/components/ContentCard.tsx
@@ -2,13 +2,15 @@
 import '../styles/ContentCard.css';
 
 type Props = {
+  children?: React.ReactNode;
   imageUrl?: string;
+  variant?: 'default' | 'plain';
 };
 
-export default function ContentCard({ imageUrl }: Props) {
+export default function ContentCard({ children,imageUrl,variant = 'default' }: Props){
   return (
-    <div className="content-card fade-in-up">
-      {imageUrl && <img src={imageUrl} className="card-image" />}
+    <div className={`content-card fade-in-up ${variant === 'plain' ? 'plain-card' : ''}`}>
+      {children ? children : imageUrl && <img src={imageUrl} alt="card" className="card-image" />}
     </div>
   );
 }
