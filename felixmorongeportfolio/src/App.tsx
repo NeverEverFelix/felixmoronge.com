3a7891639ad020a3ba8da66f36326670b8f5349e
NeverEverFelix <felixmftc@icloud.com>
2025-05-12 22:36:22 -0400

    landing page done

--- felixmorongeportfolio/src/App.tsx
+++ felixmorongeportfolio/src/App.tsx
@@ -1,21 +1,24 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
+import './styles/landing.css';
 
-function App() {
+
+
+
+export default function App() {
   return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/resume" element={<Resume />} />
-      </Routes>
-    </Router>
-  )
+    <div className="app-container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
-
-export default App
