f637b9f5a6f46d4f47001b15f28ab7b824b0e0ae
NeverEverFelix <felixmftc@icloud.com>
2025-06-16 15:46:27 -0400

    further DevOps portfolio case study

--- felixmorongeportfolio/src/pages/Projects.tsx
+++ felixmorongeportfolio/src/pages/Projects.tsx
@@ -1,8 +1,23 @@
 import '../styles/Projects.css';
 export default function Projects() {
     return (
-      <div className="text-white bg-black h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Projects Page</h1>
+      <div className="projects-main">
+        <div className="section-header">
+          <div className="development-pill">Live</div>
+            <h1 className="title">Building Enterprise-Grade CI/CD for My Portfolio Site</h1>
+            <p className="title-subtext">
+            How I architected and deployed a production-grade DevOps pipeline from scratch<br />
+            using AWS, Terraform, and Kubernetes
+            </p>
+          </div>
+        
+      
+
+      <div className="image">
+
+      </div>
+
+      <div className="divider"></div>
       </div>
     );
   }
