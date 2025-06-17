f39326d73abcb67faf0f741656bb6a40a8928181
NeverEverFelix <felixmftc@icloud.com>
2025-06-17 13:58:29 -0400

    #1 case study overview section complete

--- felixmorongeportfolio/src/pages/Projects.tsx
+++ felixmorongeportfolio/src/pages/Projects.tsx
@@ -21,6 +21,25 @@ export default function Projects() {
       <div className="divider-line left"></div>
       <div className="divider-line right"></div>
       </div>
+     <div className="overview">
+  <h2 className="overview-title-text">Overview</h2>
+  <p className="overview-subtext">
+    Initial DevOps challenges, bottlenecks, security risks, and scalability goals; what wasn’t working before
+  </p>
+  <div className="overview-maintext">
+    <p>
+      Before this project, my personal portfolio was deployed via basic hosting with no CI/CD pipeline, no monitoring, and no scalable architecture. It couldn’t handle updates safely, lacked SSL, and was entirely manual — which meant longer iteration cycles and zero rollback protection.
+    </p>
+    <p>
+      I decided to rebuild the entire infrastructure stack to reflect what I'd deploy at a mid-to-large tech company: a fully containerized frontend, built with Vite + Nginx, deployed to a Kubernetes cluster (EKS), with image builds handled by Kaniko and all provisioning done via Terraform.
+    </p>
+    <p>
+      The goal was simple: treat my portfolio like a real production-grade web service — and use it to showcase DevOps principles at scale.
+    </p>
+  </div>
+  <div className="divider full"></div>
+</div>
+
       </div>
     );
   }
