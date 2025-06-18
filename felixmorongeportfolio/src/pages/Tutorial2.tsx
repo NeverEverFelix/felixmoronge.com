feb1b2c34a7909b1ec933bb73c3429d757ea882f
NeverEverFelix <felixmftc@icloud.com>
2025-06-18 12:27:18 -0400

    #2 case study rough draft start

---
+++ felixmorongeportfolio/src/pages/Tutorial2.tsx
@@ -0,0 +1,8 @@
+import '../styles/Tutorial2.css';
+import CodeBlock from '../components/CodeBlock';
+
+export default function Tutorial2 (){
+    return(
+        <div className="divider-casestudy full" />
+    );
+}
