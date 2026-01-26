04b2fc74f5d5ff41f541896fd01bd89e6ad7ee0b
NeverEverFelix <felixmftc@icloud.com>
2026-01-26 02:27:13 -0500

    yy

--- felixmorongeportfolio/vite.config.ts
+++ felixmorongeportfolio/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages project site base path:
+  // https://nevereverfelix.github.io/felixmoronge.com/
+  base: '/felixmoronge.com/',
   css: {
     postcss: './postcss.config.cjs',
   },
@@ -13,7 +16,7 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-});
+      },
+    },
+  },
+})
